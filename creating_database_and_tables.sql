cbf45fa3441c74c9b36dd13b2c08431e55c43181
ahmedgamalbakr <106693415+ahmedgamalbakr@users.noreply.github.com>
2022-06-01 23:36:40 +0200

    Update creating_database_and_tables.sql

--- creating_database_and_tables.sql
+++ creating_database_and_tables.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE my_first_db;
 
 -- create table
-CREATE TABLE teachers ( id bigserial,
+CREATE TABLE teachers ( id serial,
 first_name varchar(25), last_name varchar(50), school varchar(50),
 hire_date date,
 salary numeric
@@ -10,12 +10,12 @@ salary numeric
 
 -- insert data in table
 INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
-VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200), 
-('Lee', 'Reynolds', 'F.D. Roosevelt HS', '1993-05-22', 65000), 
-('Samuel', 'Cole', 'Myers Middle School', '2005-08-01', 43500), 
-('Samantha', 'Bush', 'Myers Middle School', '2011-10-30', 36200), 
-('Betty', 'Diaz', 'Myers Middle School', '2005-08-30', 43500),
-('Kathleen', 'Roush', 'F.D. Roosevelt HS', '2010-10-22', 38500);
+VALUES ('Ahmed', 'Gamal', 'Roosevelt HS', '2011-10-30', 36200), 
+('Salah', 'Mossad', 'Roosevelt HS', '1993-05-22', 65000), 
+('Mossad', 'Nagy', 'Middle School', '2005-08-01', 43500), 
+('Gamal', 'Fathy', 'Myers Middle School', '2011-10-30', 36200), 
+('Waleed', 'Mostafa', 'Myers Middle School', '2005-08-30', 43500),
+('Ame', 'Shokery', 'Roosevelt HS', '2010-10-22', 38500);
 
 -- Data Exploration With Select
 SELECT * FROM teachers;
